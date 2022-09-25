828fcc4736bd08c93cc3da1f26fa4dc3ae62bd13
navneeet94 <98764448+navneeet94@users.noreply.github.com>
2022-09-25 20:22:57 +0530

    update code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  { path: '',   redirectTo: '/resume', pathMatch: 'full' },
-  {
-    path: 'resume',
-    loadChildren: () => import('./resume/resume.module').then(m => m.ResumeModule)
-  },
-  {
-    path: 'admin',
-    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
-  },
-  {
-    path: 'plugin-demo',
-    loadChildren: () => import('./plugin-demo/plugin-demo.module').then(m => m.PluginDemoModule)
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
